7d43bd250a145797c0a579d15e6795b47bbcae17
Alexey Luchkovskiy <luchkovskialexey@gmail.com>
2023-03-29 22:39:50 +0300

    Rest UserController added

---
+++ src/main/java/com/luchkovskiy/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.luchkovskiy.controller;
+
+
+import com.luchkovskiy.models.User;
+import com.luchkovskiy.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> read(@PathVariable("id") Long id) {
+        User user = userService.read(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> readAll() {
+        List<User> users = userService.readAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody User user) {
+        User createdUser = userService.create(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.OK);
+    }
+
+    @PatchMapping("")
+    public ResponseEntity<Object> update(@RequestBody User user) {
+        User updatedUser = userService.update(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        userService.delete(id);
+    }
+
+}
